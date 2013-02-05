cd1127e4c77088ddaa656f258d737bfacd2c00f8
mastarink <mastarink@mastarink.net>
2013-02-05 13:41:46 +0200

    added misc files

---
+++ sh/list-useful-files.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+ls -1 {{zocromas,commonlibs/mas_{wrap,tools,io,channel}}/{gvim-*,{g,}vimrc-*,missing,depcomp,configure,config.{h,guess,status,sub},libtool,install-sh,libtool,ltmain.sh,Makefile{.in,.am,},*.{ac,m4},m4/*,mased/*,src/{Makefile{.in,.am,},*.c,inc/{Makefile{.in,.am,},*.h}}},zocromas/src/*/{Makefile{.in,.am,},*.c,inc/{Makefile{.in,.am,},*.h}}} | sort
