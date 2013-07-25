5adcdd8baa1e8a19df37c34418bb0566d4b2a8ba
mastarink <mastarink@mastarink.net>
2013-07-25 12:40:41 +0300

    +

--- projects/commonlibs/zocromas_mas_init/src/inc/mas_opts_save.h
+++
@@ -1,7 +0,0 @@
-#ifndef MAS_OPTS_SAVE_H
-#  define MAS_OPTS_SAVE_H
-
-int mas_opts_save_user( mas_options_t * popts,  const char *dirname, const char *filename );
-int mas_opts_save_user_plus( mas_options_t * popts,  const char *dirname, const char *filename, ... );
-
-#endif
--- projects/commonlibs/zocromas_mas_init/src/inc/mas_opts_storage.h
+++
@@ -1,8 +0,0 @@
-#ifndef MAS_OPTS_STORAGE_H
-#  define MAS_OPTS_STORAGE_H
-
-int mas_opts_set_configdir( mas_options_t * popts,  const char *dirname );
-int mas_opts_set_configfilename( mas_options_t * popts,  const char *filename );
-int mas_opts_check_dir( mas_options_t * popts );
-
-#endif
