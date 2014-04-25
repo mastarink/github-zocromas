431448bc7310d73831b5dab05c3513f0fc1f2946
mastarink <mastarink@mastarink.net>
2014-04-25 09:13:45 +0300

    +; added uninstall to shn command

--- projects/bins/zocromas_duf/src/inc/duf_mdpath.h
+++
@@ -1,8 +0,0 @@
-#ifndef MAS_DUF_MDPATH_H
-#  define MAS_DUF_MDPATH_H
-
-unsigned long long duf_insert_mdpath( unsigned long long *md64 );
-
-int duf_update_mdpaths( unsigned long long pathid );
-
-#endif
