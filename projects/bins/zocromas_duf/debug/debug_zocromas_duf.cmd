210be7016d69a2499cebe79417ece09c7d686268
mastarink <mastarink@mastarink.net>
2014-05-04 01:46:53 +0300

    +

--- projects/bins/zocromas_duf/debug/debug_zocromas_duf.cmd
+++ projects/bins/zocromas_duf/debug/debug_zocromas_duf.cmd
@@ -3,6 +3,7 @@ define hook-quit
     set confirm off
 end
 handle SIGPIPE nostop noprint
-b duf_levinfo.c:554
+b src/duf_sqlite.c:164
+b src/duf_action.c:70
 run
 bt
--- projects/bins/zocromas_duf/editing/mased/srcsql.mased.vim
+++ projects/bins/zocromas_duf/editing/mased/srcsql.mased.vim
@@ -1,5 +1,7 @@
 find duf_sql.c
 sfind duf_sql.h
+sfind duf_sql1.h
+sfind duf_sql2.h
 sfind duf_sql_def.h
 
 tab sfind duf_sqlite.c
