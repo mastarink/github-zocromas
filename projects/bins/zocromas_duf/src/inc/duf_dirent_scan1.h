a81d38e10be0d02418276e1091a28f46b9a60236
mastarink <mastarink@mastarink.net>
2014-08-26 15:52:18 +0300

    +

--- projects/bins/zocromas_duf/src/inc/duf_dirent_scan1.h
+++
@@ -1,8 +0,0 @@
-#ifndef MAS_DUF_DIRENT_SCAN1_H
-#  define MAS_DUF_DIRENT_SCAN1_H
-
-int duf_scan_dirents1( duf_depthinfo_t * pdi, duf_record_t * precord, duf_scan_hook_dirent_reg_t scan_dirent_reg,
-                       duf_scan_hook_dirent_dir_t scan_dirent_dir );
-
-
-#endif
