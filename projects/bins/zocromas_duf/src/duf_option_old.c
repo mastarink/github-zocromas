9663a068cd8f7b85435fae8ef7155ccb334fafc6
Michael Starynkevitch <mastarink@gmail.com>
2015-06-03 12:53:52 +0300

    duf_option_old.c - removed commented-out expired parts

--- projects/bins/zocromas_duf/src/duf_option_old.c
+++ projects/bins/zocromas_duf/src/duf_option_old.c
@@ -20,93 +20,7 @@
 #include "duf_option_old.h"
 /* ###################################################################### */
 
-#if 0
-static int
-duf_limits( const char *s, unsigned *pmin, unsigned *pmax )
-{
-  DEBUG_STARTR( r );
 
-  if ( s )
-  {
-    long n;
-    char c = 0;
-
-    if ( *s == '+' )
-      c = *s++;
-    else if ( *s == '-' )
-      c = *s++;
-    n = duf_strtol_suff( s, &r );
-    if ( r >= 0 )
-    {
-      if ( c == '+' )
-      {
-        if ( pmin )
-          *pmin = n + 1;
-      }
-      else if ( c == '-' )
-      {
-        if ( pmax )
-          *pmax = n - 1;
-      }
-      else
-      {
-        if ( pmin )
-          *pmin = n;
-        if ( pmax )
-          *pmax = n;
-      }
-    }
-    else
-      DUF_MAKE_ERROR( r, DUF_ERROR_OPTION_VALUE );
-    DUF_SHOW_ERROR( "[%c] %d - %d", c, pmin ? *pmin : 0, pmax ? *pmax : 0 );
-  }
-  DEBUG_ENDR( r );
-}
-#endif
-
-#if 0
-static int
-duf_limitsll( const char *s, unsigned long long *pmin, unsigned long long *pmax )
-{
-  DEBUG_STARTR( r );
-
-  if ( s )
-  {
-    long n;
-    char c = 0;
-
-    if ( *s == '+' )
-      c = *s++;
-    else if ( *s == '-' )
-      c = *s++;
-    n = duf_strtol_suff( s, &r );
-    if ( r >= 0 )
-    {
-      if ( c == '+' )
-      {
-        if ( pmin )
-          *pmin = n + 1;
-      }
-      else if ( c == '-' )
-      {
-        if ( pmax )
-          *pmax = n - 1;
-      }
-      else
-      {
-        if ( pmin )
-          *pmin = n;
-        if ( pmax )
-          *pmax = n;
-      }
-    }
-    else
-      DUF_MAKE_ERROR( r, DUF_ERROR_OPTION_VALUE );
-    DUF_SHOW_ERROR( "[%c] %lld - %lld", c, pmin ? *pmin : 0, pmax ? *pmax : 0 );
-  }
-  DEBUG_ENDR( r );
-}
-#endif
 
 int
 duf_parse_option_long_old( const duf_longval_extended_t * extended, const char *optargg, duf_option_stage_t istage,
@@ -114,78 +28,7 @@ duf_parse_option_long_old( const duf_longval_extended_t * extended, const char *
 {
   DEBUG_STARTR( r );
 
-  if ( extended )
-    switch ( extended->oclass )
-    {
-    case DUF_OPTION_CLASS_ANY:
-      /* r = 0; */
-      break;
-    case DUF_OPTION_CLASS_MIN:
-      /* r = 0; */
-      break;
-    case DUF_OPTION_CLASS_NONE:
-      /* r = 0; */
-      break;
-    case DUF_OPTION_CLASS_NO_HELP:
-      /* r = 0; */
-      break;
-    case DUF_OPTION_CLASS_HELP:
-      /* r = 0; */
-      break;
-    case DUF_OPTION_CLASS_SYSTEM:
-      /* r = 0; */
-      break;
-    case DUF_OPTION_CLASS_CONTROL:
-      /* r = 0; */
-      break;
-    case DUF_OPTION_CLASS_REFERENCE:
-      /* r = 0; */
-      break;
-    case DUF_OPTION_CLASS_COLLECT:
-      /* r = 0; */
-      break;
-    case DUF_OPTION_CLASS_SCAN:
-      /* r = 0; */
-      break;
-    case DUF_OPTION_CLASS_FILTER:
-      /* r = 0; */
-      break;
-    case DUF_OPTION_CLASS_UPDATE:
-      /* r = 0; */
-      break;
-    case DUF_OPTION_CLASS_REQUEST:
-      /* r = 0; */
-      break;
-    case DUF_OPTION_CLASS_PRINT:
-      /* r = 0; */
-      break;
-    case DUF_OPTION_CLASS_TRACE:
-      /* r = 0; */
-      break;
-    case DUF_OPTION_CLASS_DEBUG:
-      /* r = 0; */
-      break;
-    case DUF_OPTION_CLASS_OBSOLETE:
-      /* r = 0; */
-      break;
-    case DUF_OPTION_CLASS_NODESC:
-      /* r = 0; */
-      break;
-    case DUF_OPTION_CLASS_OTHER:
-      /* r = 0; */
-      break;
-    case DUF_OPTION_CLASS_MAX:
-      /* r = 0; */
-      break;
-    case DUF_OPTION_CLASS_BAD:
-      /* r = 0; */
-      break;
-    case DUF_OPTION_CLASS_ALL:
-      /* r = 0; */
-      break;
-    }
-  else
-    DUF_MAKE_ERROR( r, DUF_ERROR_OPTION );
+
 
   DUF_TRACE( options, 2, "(x%d) to parse %s (%s)  (%d)%s  cv:%d (F:%d)", extended ? 1 : 0, duf_option_description_x_tmp( -1, extended, NULL ),
              extended->o.name, r, duf_error_name( r ), extended->o.val, DUF_OPTION_VAL_BFORMAT );
@@ -196,115 +39,9 @@ duf_parse_option_long_old( const duf_longval_extended_t * extended, const char *
     {
     case DUF_OPTION_VAL_NONE:
       break;
-      /* case DUF_OPTION_VAL_SMART_HELP:     */
-      /*                                 */
-      /* case DUF_OPTION_VAL_HELP_ALL:       */
-      /* case DUF_OPTION_VAL_HELP_NONE:      */
-      /*                                 */
-      /* case DUF_OPTION_VAL_HELP_HELP:      */
-      /* case DUF_OPTION_VAL_HELP_SYSTEM:    */
-      /* case DUF_OPTION_VAL_HELP_CONTROL:   */
-      /* case DUF_OPTION_VAL_HELP_REFERENCE: */
-      /* case DUF_OPTION_VAL_HELP_COLLECT:   */
-      /* case DUF_OPTION_VAL_HELP_SCAN:      */
-      /* case DUF_OPTION_VAL_HELP_UPDATE:    */
-      /* case DUF_OPTION_VAL_HELP_REQUEST:   */
-      /* case DUF_OPTION_VAL_HELP_PRINT:     */
-      /*                                 */
-      /* case DUF_OPTION_VAL_HELP_TRACE:     */
-      /* case DUF_OPTION_VAL_HELP_DEBUG:     */
-      /* case DUF_OPTION_VAL_HELP_OBSOLETE:  */
-      /* case DUF_OPTION_VAL_HELP_NODESC:    */
-/* .................................................. */
-      /* case DUF_OPTION_VAL_HELP:                               */
-      /* case DUF_OPTION_VAL_EXAMPLES:                           */
-      /* case DUF_OPTION_VAL_VERSION:                            */
-      /*   if ( optargg )                                     */
-      /*   {                                                 */
-      /*     if ( duf_config->help_string )                  */
-      /*       mas_free( duf_config->help_string );          */
-      /*     duf_config->help_string = mas_strdup( optargg ); */
-      /*   }                                                 */
-      /*   DUF_PRINTF( 0, "vvvvvvvvvvvv-" );                 */
-      /*   break;                                            */
     case DUF_OPTION_VAL_TEST:
       DUF_PRINTF( 0, "This is test option output; optargg:%s", optargg ? optargg : "-" );
       break;
-/* */
-      /* DUF_OPTION_CASE_ACQUIRE_NUM( OUTPUT_LEVEL, (*             *) level, (*     *) cli.output ); */
-
-
-/* debug etc. */
-      /* DUF_OPTION_CASE_ACQUIRE_NUM( VERBOSE, (*            *) verbose, (*         *) cli.dbg ); */
-      /* DUF_OPTION_CASE_ACQUIRE_NUM( DEBUG, (*              *) debug, (*           *) cli.dbg ); */
-      /* DUF_OPTION_CASE_ACQUIRE_NUM( MIN_DBGLINE, (*        *) min_line, (*        *) cli.dbg ); */
-      /* DUF_OPTION_CASE_ACQUIRE_NUM( MAX_DBGLINE, (*        *) max_line, (*        *) cli.dbg ); */
-
-
-
-/* db */
-      /* DUF_OPTION_CASE_ACQUIRE_STR( DB_DIRECTORY, (*           *) dir, (*             *) db ); */
-      /* DUF_OPTION_CASE_ACQUIRE_STR( DB_NAME_MAIN, (*           *) main.name, (*       *) db ); */
-      /* DUF_OPTION_CASE_ACQUIRE_STR( DB_NAME_ADM, (*            *) adm.name, (*        *) db ); */
-
-      /* DUF_OPTION_CASE_ACQUIRE_ACT_FLAG( UNI_SCAN, (*          *) uni_scan (*     *)  ); */
-      /* DUF_OPTION_CASE_ACQUIRE_ACT_FLAG( INFO, (*              *) info (*         *)  ); */
-
-      /* DUF_OPTION_CASE_ACQUIRE_ACT_FLAG( REMOVE_DATABASE, (*   *) remove_database (* *)  ); */
-      /* DUF_OPTION_CASE_ACQUIRE_ACT_FLAG( DROP_TABLES, (*       *) drop_tables (*     *)  ); */
-      /* DUF_OPTION_CASE_ACQUIRE_ACT_FLAG( CREATE_TABLES, (*     *) create_tables (*   *)  ); */
-      /* DUF_OPTION_CASE_ACQUIRE_ACT_FLAG( ADD_PATH, (*          *) add_path (*        *)  ); */
-      /* DUF_OPTION_CASE_ACQUIRE_ACT_FLAG( FILEDATA, (*          *) filedata (*        *)  ); */
-      /* DUF_OPTION_CASE_ACQUIRE_ACT_FLAG( FILENAMES, (*         *) filenames (*       *)  ); */
-      /* DUF_OPTION_CASE_ACQUIRE_ACT_FLAG( VACUUM, (*            *) vacuum (*          *)  ); */
-
-/* actions */
-      /* DUF_OPTION_CASE_ACQUIRE_ACT_FLAG( PROGRESS, (*          *) progress (*        *)  ); */
-      /* DUF_OPTION_CASE_ACQUIRE_ACT_FLAG( SUMMARY, (*           *) summary (*         *)  ); */
-
-      /* DUF_OPTION_CASE_ACQUIRE_ACT_NUM_PLUS( SAMPLE, (*        *) sample (*          *)  ); */
-      /* DUF_OPTION_CASE_ACQUIRE_ACT_NUM_PLUS( SAMPUPD, (*       *) sampupd (*         *)  ); */
-
-      /* DUF_OPTION_CASE_ACQUIRE_ACT_FLAG( MDPATH, (*            *) mdpath (*          *)  ); */
-      /* DUF_OPTION_CASE_ACQUIRE_ACT_FLAG( DIRENT, (*            *) dirent (*          *)  ); */
-      /* DUF_OPTION_CASE_ACQUIRE_ACT_FLAG( SD5, (*               *) sd5 (*             *)  ); */
-      /* DUF_OPTION_CASE_ACQUIRE_ACT_FLAG( MD5, (*               *) md5 (*             *)  ); */
-      /* DUF_OPTION_CASE_ACQUIRE_ACT_FLAG( CRC32, (*             *) crc32 (*           *)  ); */
-      /* DUF_OPTION_CASE_ACQUIRE_ACT_FLAG( MIME, (*              *) mime (*            *)  ); */
-      /* DUF_OPTION_CASE_ACQUIRE_ACT_FLAG( EXIF, (*              *) exif (*            *)  ); */
-
-      /* DUF_OPTION_CASE_ACQUIRE_ACT_FLAG( COLLECT, (*           *) collect (*         *)  ); */
-      /* DUF_OPTION_CASE_ACQUIRE_ACT_FLAG( INTEGRITY, (*         *) integrity (*       *)  ); */
-      /* DUF_OPTION_CASE_ACQUIRE_ACT_FLAG( PRINT, (*             *) print (*           *)  ); */
-      /* DUF_OPTION_CASE_ACQUIRE_ACT_FLAG( TREE, (*              *) tree (*            *)  ); */
-      /* DUF_OPTION_CASE_ACQUIRE_ACT_FLAG( DIRS, (*              *) dirs (*            *)  ); */
-      /* DUF_OPTION_CASE_ACQUIRE_ACT_FLAG( FILES, (*             *) files (*           *)  ); */
-
-      /* DUF_OPTION_CASE_ACQUIRE_FLAGG( DISABLE_CALCULATE, (*    *) calculate, (*      *) cli,.disable ); */
-      /* DUF_OPTION_CASE_ACQUIRE_FLAGG( DISABLE_INSERT, (*       *) insert, (*         *) cli,.disable ); */
-      /* DUF_OPTION_CASE_ACQUIRE_FLAGG( DISABLE_UPDATE, (*       *) update, (*         *) cli,.disable ); */
-
-      /* DUF_OPTION_CASE_ACQUIRE_U_FLAG( RECURSIVE, (*         *) recursive (*       *)  ); */
-
-      /* DUF_OPTION_CASE_ACQUIRE_U_NUM( SD5ID, (*              *) sd5id (*           *)  ); */
-      /* DUF_OPTION_CASE_ACQUIRE_U_NUM( MD5ID, (*              *) md5id (*           *)  ); */
-      /* DUF_OPTION_CASE_ACQUIRE_U_NUM( CRC32ID, (*            *) crc32id (*         *)  ); */
-      /* DUF_OPTION_CASE_ACQUIRE_U_NUM( EXIFID, (*             *) exifid (*          *)  ); */
-      /* DUF_OPTION_CASE_ACQUIRE_U_NUM( MIMEID, (*             *) mimeid (*          *)  ); */
-
-/* limits, filters, selectors */
-      /* case DUF_OPTION_SIZE:                                                           */
-      /*   r = duf_limitsll( optargg, &duf_config->u.size.min, &duf_config->u.size.max ); */
-      /*   break;                                                                        */
-      /* DUF_OPTION_CASE_ACQUIRE_U_NUM( MAXSIZE, (*            *) size.max (*     *)  ); */
-      /* DUF_OPTION_CASE_ACQUIRE_U_NUM( MINSIZE, (*            *) size.min (*     *)  ); */
-
-      /* case DUF_OPTION_SAME:                                                         */
-      /*   r = duf_limits( optargg, &duf_config->u.same.min, &duf_config->u.same.max ); */
-      /*   break;                                                                      */
-      /* DUF_OPTION_CASE_ACQUIRE_U_NUM( MAXSAME, (*            *) same.max (*     *)  ); */
-      /* DUF_OPTION_CASE_ACQUIRE_U_NUM( MINSAME, (*            *) same.min (*     *)  ); */
-
       DUF_OPTION_CASE_ACQUIRE_U_NUM( MAXDIRFILES, /*        */ dirfiles.max /*     */  );
       DUF_OPTION_CASE_ACQUIRE_U_NUM( MINDIRFILES, /*        */ dirfiles.min /*     */  );
 
@@ -319,42 +56,6 @@ duf_parse_option_long_old( const duf_longval_extended_t * extended, const char *
       DUF_OPTION_CASE_ACQUIRE_U_ARG( GLOB_INCL_FS_FILES, /* */ globx.include_fs_files /*     */  );
       DUF_OPTION_CASE_ACQUIRE_U_ARG( GLOB_EXCL_FS_FILES, /* */ globx.exclude_fs_files /*     */  );
 
-
-/* trace */
-      /* DUF_OPTION_CASE_ACQUIRE_NUM( TRACE_NONEW, (*        *) nonew, (*     *) cli.trace ); */
-
-      /* DUF_OPTION_CASE_ACQUIRE_TRACE( DRY_RUN, (*           *) dry_run ); */
-      /* DUF_OPTION_CASE_ACQUIRE_TRACE( EXPLAIN, (*           *) explain ); */
-
-      /* DUF_OPTION_CASE_ACQUIRE_TRACE( SEQ, (*              *) seq ); */
-      /* DUF_OPTION_CASE_ACQUIRE_TRACE( OPTIONS, (*          *) options ); */
-      /* DUF_OPTION_CASE_ACQUIRE_TRACE( CALLS, (*            *) calls ); */
-      /* DUF_OPTION_CASE_ACQUIRE_TRACE( ANY, (*              *) any ); */
-      /* DUF_OPTION_CASE_ACQUIRE_TRACE( CURRENT, (*          *) current ); */
-      /* DUF_OPTION_CASE_ACQUIRE_TRACE( ACTION, (*           *) action ); */
-      /* DUF_OPTION_CASE_ACQUIRE_TRACE( ERROR, (*            *) error ); */
-      /* DUF_OPTION_CASE_ACQUIRE_TRACE( SCAN, (*             *) scan ); */
-      /* DUF_OPTION_CASE_ACQUIRE_TRACE( SCAN_DE_DIR, (*      *) scan_de_dir ); */
-      /* DUF_OPTION_CASE_ACQUIRE_TRACE( SCAN_DE_REG, (*      *) scan_de_reg ); */
-      /* DUF_OPTION_CASE_ACQUIRE_TRACE( TEMP, (*             *) temp ); */
-      /* DUF_OPTION_CASE_ACQUIRE_TRACE( PATH, (*             *) path ); */
-      /* DUF_OPTION_CASE_ACQUIRE_TRACE( FS, (*               *) fs ); */
-      /* DUF_OPTION_CASE_ACQUIRE_TRACE( DELETED, (*          *) deleted ); */
-      /* DUF_OPTION_CASE_ACQUIRE_TRACE( SAMPUPD, (*          *) sampupd ); */
-      /* DUF_OPTION_CASE_ACQUIRE_TRACE( SAMPLE, (*           *) sample ); */
-      /* DUF_OPTION_CASE_ACQUIRE_TRACE( MDPATH, (*           *) mdpath ); */
-      /* DUF_OPTION_CASE_ACQUIRE_TRACE( DIRENT, (*           *) dirent ); */
-      /* DUF_OPTION_CASE_ACQUIRE_TRACE( SD5, (*              *) sd5 ); */
-      /* DUF_OPTION_CASE_ACQUIRE_TRACE( MD5, (*              *) md5 ); */
-      /* DUF_OPTION_CASE_ACQUIRE_TRACE( CRC32, (*            *) crc32 ); */
-      /* DUF_OPTION_CASE_ACQUIRE_TRACE( MIME, (*             *) mime ); */
-      /* DUF_OPTION_CASE_ACQUIRE_TRACE( EXIF, (*             *) exif ); */
-      /* DUF_OPTION_CASE_ACQUIRE_TRACE( COLLECT, (*          *) collect ); */
-      /* DUF_OPTION_CASE_ACQUIRE_TRACE( INTEGRITY, (*        *) integrity ); */
-      /* DUF_OPTION_CASE_ACQUIRE_TRACE( SQL, (*              *) sql ); */
-      /* DUF_OPTION_CASE_ACQUIRE_TRACE( SELECT, (*           *) select ); */
-      /* DUF_OPTION_CASE_ACQUIRE_TRACE( INSERT, (*           *) insert ); */
-      /* DUF_OPTION_CASE_ACQUIRE_TRACE( UPDATE, (*           *) update ); */
 
 /* i/o */
 
@@ -388,13 +89,6 @@ duf_parse_option_long_old( const duf_longval_extended_t * extended, const char *
       duf_config->cli.trace.output.out = stdout;
       r = 0;
       break;
-      /* case DUF_OPTION_VAL_TRACE_FILE:                                                                              */
-      /*   r = duf_open_special( optargg, &duf_config->cli.trace.output.file, &duf_config->cli.trace.output.out ); */
-      /*   break;                                                                                                 */
-      /*                                                                                                          */
-      /* case DUF_OPTION_VAL_OUTPUT_FILE:                                                                             */
-      /*   r = duf_open_special( optargg, &duf_config->cli.output.file, &duf_config->cli.output.out );             */
-      /*   break;                                                                                                 */
 
 /* combined */
     case DUF_OPTION_VAL_ALL_TRACE:
