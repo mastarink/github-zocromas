68b164b0bfefb7fe4bb1ed8437893c652e9b6588
Michael Starynkevitch <mastarink@gmail.com>
2015-09-17 11:45:22 +0300

    duf_sql2.h => duf_sql_positional.h; removed duf_sccb_handle.c for count and duf_sql_field.c, naturally

---
+++ projects/bins/zocromas_duf/src/duf_sql_positional.c
@@ -0,0 +1,40 @@
+#include <assert.h>
+
+#include <mastar/wrap/mas_std_def.h>
+#include <mastar/wrap/mas_memory.h>
+#include <mastar/sqlite/mas_sqlite.h>
+#include <mastar/sqlite/mas_sqlite_const.h>
+
+
+#include "duf_maintenance.h"
+
+#include "duf_service.h"
+
+#include "duf_config_ref.h"
+
+#include "duf_sql_error.h"
+
+#include "duf_sqlite_select.h"
+
+/* ###################################################################### */
+#include "duf_sql_positional.h"
+/* ###################################################################### */
+
+
+int
+duf_sql_column_int( mas_sqlite_stmt_t * stmt, int icol )
+{
+  return mas_sqlite_column_int( stmt, icol );
+}
+
+long long
+duf_sql_column_long_long( mas_sqlite_stmt_t * stmt, int icol )
+{
+  return mas_sqlite_column_long_long( stmt, icol );
+}
+
+const char *
+duf_sql_column_string( mas_sqlite_stmt_t * stmt, int icol )
+{
+  return mas_sqlite_column_string( stmt, icol );
+}
