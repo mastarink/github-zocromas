bf67542290e555c9df7192be47d260874d078ee2
Michael Starynkevitch <mastarink@gmail.com>
2015-10-16 19:05:02 +0300

    Mrak & Dron; removed duf_utils_print.[ch]

--- projects/bins/zocromas_duf/editing/mased/srclow.mased.vim
+++ projects/bins/zocromas_duf/editing/mased/srclow.mased.vim
@@ -10,8 +10,8 @@ sfind duf_utils.h
 tab sfind duf_utils_path.c
 sfind duf_utils_path.h
 
-tab sfind duf_utils_print.c
-sfind duf_utils_print.h
+" tab sfind duf_utils_print.c
+" sfind duf_utils_print.h
 
 tab sfind duf_match.c
 sfind duf_match.h
--- projects/bins/zocromas_duf/src/inc/duf_maintenance_options.h
+++ projects/bins/zocromas_duf/src/inc/duf_maintenance_options.h
@@ -2,24 +2,7 @@
 #  define MAS_DUF_MAINTENANCE_OPTIONS_H
 #  define DUF_T_NOIF
 
-#  if 0
-#    include <assert.h>
 
-#    include <mastar/wrap/mas_std_def.h>
-#    include <mastar/wrap/mas_memory.h>
-
-#    include "duf_start_end.h"
-#    include "duf_dodefs.h"
-#    include "duf_errors.h"
-#    include "duf_error_defs.h"
-
-#    include "duf_trace.h"
-#    include "duf_utils_print.h"
-
-#    include "duf_debug_defs.h"
-#    include "duf_dbg.h"
-#  else
-#    include "duf_maintenance.h"
-#  endif
+#  include "duf_maintenance.h"
 
 #endif
