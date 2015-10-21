110d9e9b140e6551ebb85cf83c2a15b3a4046b23
Michael Starynkevitch <mastarink@gmail.com>
2015-10-21 13:50:00 +0300

    Mrak & Dron; DUF_USE_TMP_PATHTOT...

---
+++ projects/bins/zocromas_duf/src/inc/sql_tables_global_defs.h
@@ -0,0 +1,31 @@
+#ifndef MAS_SQL_TABLES_GLOBAL_DEFS_H
+#  define MAS_SQL_TABLES_GLOBAL_DEFS_H
+
+#  define DUF_USE_CTE yes
+
+/* #  define DUF_USE_TMP_PATHTOT yes */
+#  ifdef DUF_USE_TMP_PATHTOT
+#    define DUF_USE_TMP_PATHTOT_DIRS_TABLE yes
+#    define DUF_USE_TMP_PATHTOT_FILES_TABLE yes
+#  endif
+
+#  define DUF_SQL_SELECTED_TEMPORARY "TEMPORARY"
+#  define DUF_SQL_TTABLES_TEMPORARY "TEMPORARY"
+/* #  define DUF_SQL_TABLES_TEMPORARY "TEMPORARY" */
+
+#  define DUF_SQL_TABLES_FILENAMES             "filenames"
+#  define DUF_SQL_TABLES_FILEDATAS             "filedatas"
+#  define DUF_SQL_TABLES_SHA1                  "sha1"
+#  define DUF_SQL_TABLES_MD5                   "md5"
+#  define DUF_SQL_TABLES_SD5                   "sd5"
+#  define DUF_SQL_TABLES_CRC32                 "crc32"
+#  define DUF_SQL_TABLES_SIZES                 "sizes"
+#  define DUF_SQL_TABLES_EXIF                  "exif"
+#  define DUF_SQL_TABLES_EXIF_MODEL            "exif_model"
+#  define DUF_SQL_TABLES_MIME                  "mime"
+#  define DUF_SQL_TABLES_PATHS                 "paths"
+#  define DUF_SQL_TABLES_TAGS                  "tags"
+#  define DUF_SQL_TABLES_TAGNAMES              "tagnames"
+
+
+#endif
