1504613f5a1c424d99d497af9ef94ba82c97400f
Michael Starynkevitch <mastarink@gmail.com>
2018-01-02 13:03:35 +0200

    Mrak & Dron...

--- projects/commonlibs/zocromas_levinfo/src/masxfs_levinfo_format.c
+++ projects/commonlibs/zocromas_levinfo/src/masxfs_levinfo_format.c
@@ -28,14 +28,14 @@ masxfs_levinfo_prefix( masxfs_levinfo_t * li, char *p1, char *p2, char *p3, char
       len = len2;
     if ( len3 > len )
       len = len3;
-    char *pw = NULL;
+    /* char *pw = NULL; */
     masxfs_levinfo_t *lia = masxfs_levinfo_li2lia( li );
 
     if ( test > 1 )
       len = 17;
     else if ( test )
-      len = 9;
-    pw = prefix = mas_calloc( li->lidepth + 2, len );
+      len = 17 /*9*/;
+/*    pw = */prefix = mas_calloc( li->lidepth + 2, len );
     /* WARN( "%d", top_depth ); */
     for ( masxfs_depth_t d = top_depth; d < li->lidepth; d++ )
     {
@@ -47,34 +47,36 @@ masxfs_levinfo_prefix( masxfs_levinfo_t * li, char *p1, char *p2, char *p3, char
       unsigned deep = ( d == li->lidepth - 1 );
       unsigned cas = ( delta << 1 ) + deep;
 
-      if ( !test && child_count_z )
+      if ( /* !test && */child_count_z )
       {
-        if ( test )
+//      if ( test )
         {
+	  char buf[32];
           if ( test > 1 )
-            sprintf( pw, "[%3ld %3ld %3ld %3ld]", child_count_z, child_count, ( long ) li->lidepth, ( long ) d );
-          else
-            sprintf( pw, "[%15d]", cas );
-          pw += len;
+            sprintf( buf, "[%3ld %3ld %3ld %3ld]", child_count_z, child_count, ( long ) li->lidepth, ( long ) d );
+          else if (test)
+            sprintf( buf, "[%15d]", cas );
+	  strcat(prefix, buf);
         }
-        else
+//      else
         {
+	  char *pp=NULL;
           switch ( cas )
-          {
+          {	    
           case 0:
-            pw = p1;
+            pp = p1;
             break;
           case 1:
-            pw = p2;
+            pp = p2;
             break;
           case 2:
-            pw = p3;
+            pp = p3;
             break;
           case 3:
-            pw = p4;
+            pp = p4;
             break;
           }
-          strncat( prefix, pw, len );
+          strcat( prefix, pp );
         }
       }
     }
